d8211daaf9ab7df3846e93f3d1f673042fa71370
Stephen Rivera <st.rivera_94@yahoo.com>
2019-08-19 16:44:45 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -22,10 +22,9 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 def reduce_to_all_true(source_array)
-  source_array.reduce {|memo, word| true}
+if source_array.all?(true) {|word| word == true}
 end
 
 def reduce_to_any_true(source_array)
-  source_array.reduce {|memo, word| true}
-  
+  source_array.any? {|word| word == true}
 end  
