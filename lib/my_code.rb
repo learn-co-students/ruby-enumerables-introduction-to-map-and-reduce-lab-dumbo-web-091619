dd2d02f9e799406df2cf5314e58c3be389749a52
comic-engineer-3780 <ethanjosephfournier@gmail.com>
2019-09-03 17:58:53 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,28 +1,61 @@
 # My Code here....
 def map_to_negativize(source_array)
-  source_array.map{|i| i * -1}
+  result = []
+  source_array.length.times { |i|
+    result.push(source_array[i] * -1)
+  }
+  result
 end
 
 def map_to_no_change(source_array)
-  source_array.map{|i| i}
+  result = []
+  source_array.length.times { |i|
+    result.push(source_array[i])
+  }
+  result
 end
 
 def map_to_double(source_array)
-  source_array.map{|i| i * 2}
+  result = []
+  source_array.length.times { |i|
+    result.push(source_array[i] * 2)
+  }
+  result
 end
 
 def map_to_square(source_array)
-  source_array.map{|i| i * i}
+  result = []
+  source_array.length.times { |i|
+    result.push(source_array[i] * source_array[i])
+  }
+  result
 end
 
 def reduce_to_total(source_array,starting_point = 0)
-  source_array.reduce(starting_point, :+)
+  source_array.length.times { |i|
+    starting_point += source_array[i]
+  }
+  starting_point
 end
 
 def reduce_to_all_true(source_array)
-  source_array.reduce{|result,current| result = current ? true: false}
+  truthy = true
+  source_array.length.times { |i|
+    if source_array[i]
+      truthy = true
+    else
+      return false
+    end
+  }
+  truthy
 end
 
 def reduce_to_any_true(source_array)
-  source_array.reduce{|result,current| result = current ? true: false}
+  truthy = false
+  source_array.length.times { |i|
+    if source_array[i]
+      return true
+    end
+  }
+  truthy
 end
