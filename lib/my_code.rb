fd7c2789c282115d294326bf391d5ad4d634e6c1
Javone Smith <javosmith12@gmail.com>
2019-09-12 19:38:23 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,16 @@
-# My Code here....
+def map_to_negativize(array)
+  array.map {|i| i * -1}
+end
+
+def map_to_no_change(array)
+  p array
+end
+
+def map_to_double(array)
+  array.map {|i| i * 2}
+end
+
+def map_to_square(array)
+  array.map {|i| ** 2}
+
+end
