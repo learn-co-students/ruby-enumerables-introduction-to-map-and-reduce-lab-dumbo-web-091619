65f11ba3b7ceab199e372cc4661553d4fa0c19b2
awazienommy <coawazie@gmail.com>
2019-09-13 10:02:13 -0400

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,52 @@
 # My Code here....
+def map_to_negativize(array)
+    new_array =[]
+    array.each do |ele|
+        new_array << ele * -1
+    end
+    new_array 
+end
+
+def map_to_no_change(array)
+    new_array = []
+    array.each do |ele|
+        new_array << ele
+    end
+    new_array
+end
+
+def map_to_double(array)
+    new_array = []
+    array.each do |ele|
+        new_array << ele * 2
+    end
+    new_array
+end
+
+def map_to_square(array)
+    new_array = []
+    array.each do |ele|
+        new_array << ele ** 2
+    end
+    new_array
+end
+
+def reduce_to_total(array, total = 0)
+    array.each do |ele|
+        total += ele
+    end
+    total
+end
+
+def reduce_to_all_true(array)
+    array.each do |ele|
+        return false if ele == false
+    end
+end
+
+def reduce_to_any_true(array)
+    array.each do |ele|
+        return true if ele == true
+    end
+    return false
+end
