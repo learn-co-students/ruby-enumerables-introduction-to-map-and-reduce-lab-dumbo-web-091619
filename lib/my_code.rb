eaebec1bbe3c7d761da6eee114c6ba1e9993530e
Stephen Rivera <st.rivera_94@yahoo.com>
2019-09-13 13:42:33 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,8 +15,6 @@ def map_to_square(source_array)
   source_array.map{|n| n**2}
 end 
 
-
-
 def reduce_to_total(source_array, starting_point = 0) 
   source_array.reduce(starting_point) {|sum,n| sum + n}
 end
@@ -26,5 +24,5 @@ def reduce_to_all_true(source_array)
 end
 
 def reduce_to_any_true(source_array)
-  source_array.any? {|word| word == true}
-end  
+  source_array.any?
+end
